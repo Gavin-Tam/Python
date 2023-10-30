0a6c577b8bda1864ff3e3e80a14ee8443ca12579
Gavin Nguyen <147111953+Gavin-Tam@users.noreply.github.com>
2023-10-30 17:33:41 -0400

    Example of a Student Class

---
+++ Object_Student.py
@@ -0,0 +1,8 @@
+#Student Class 
+from Student import Student 
+#Object of a student class, creates a student. 
+student1 = Student("Gavin", "Software Engineering", 3.1, False)
+print(student1.name)
+print(student1.major)
+print(student1.gpa)
+print(student1.is_on_probation)
